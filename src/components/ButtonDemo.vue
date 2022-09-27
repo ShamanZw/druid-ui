0c776bb3c6d672a315e755a8216bc4b4556eaccc
Zw <druidzw@163.com>
2022-09-27 14:13:10 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <p>Button 示例</p>
+    <h1>示例1</h1>
+    <div>
+      <Button>你好</Button>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
